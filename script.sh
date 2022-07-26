6abf23d76a6c60f2aa203eb5645f6d3565784bec
Renan2010 <104217289+Renan2010@users.noreply.github.com>
2022-07-26 14:11:30 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+pacman -Syy
+pacman -S python --noconfirm
+pacman -S lua --noconfirm
+pacman -S code --noconfirm
+pacman -S firefox --noconfirm
+pacman -S nano --noconfirm
+pacman -S fish --noconfirm
+pacman -S alacrity --noconfirm
+pacman -S neovim --noconfirm
+pacman -S rust --noconfirm
+pacman -S nim --noconfirm
+pacman -S git --noconfirm
+pacman -S gcc --noconfirm
